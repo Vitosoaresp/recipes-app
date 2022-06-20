c3a9c44500a143a54d5f2fd57817fbab1b9875ab
Breno Godoy <godoyy25z@gmail.com>
2022-06-20 15:41:08 -0300

    Começo tela de login - email, senha e botão

--- src/App.js
+++ src/App.js
@@ -1,19 +1,18 @@
 import React from 'react';
 import './App.css';
-import rockGlass from './images/rockGlass.svg';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import {
+  Switch,
+  Route,
+} from 'react-router-dom';
+import Login from './pages/login';
 
 function App() {
   return (
-    <div className="meals">
-      <span className="logo">TRYBE</span>
-      <object
-        className="rocksGlass"
-        type="image/svg+xml"
-        data={ rockGlass }
-      >
-        Glass
-      </object>
+    <div>
+      <Switch>
+        <Route exact path="/" component={ Login } />
+      </Switch>
     </div>
   );
 }
