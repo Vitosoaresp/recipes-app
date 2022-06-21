e51c5e6a72cfb2635ba96e3cf0970e2878d8f432
Breno Godoy <godoyy25z@gmail.com>
2022-06-21 14:59:49 -0300

    Requisitos 11 e 12 - adiciona rota ao perfil e cria botão para barra de busca

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,16 @@
 import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 import profileIcon from '../images/profileIcon.svg';
 import searchIcon from '../images/searchIcon.svg';
 
 function Header({ title }) {
   const [bool, setBool] = useState(true);
+  const [showInput, setShowInput] = useState(false);
   useEffect(() => {
+    document.getElementById('foo').addEventListener('click', () => {
+      setShowInput(!showInput);
+    });
     if (title === 'Explore'
     || title === 'Explore Foods'
     || title === 'Explore Drinks'
@@ -15,15 +20,34 @@ function Header({ title }) {
     || title === 'Favorite Recipes') {
       setBool(false);
     }
-  }, [title]);
+  }, [title, showInput]);
 
   return (
-    <header>
-      <img src={ profileIcon } alt="Imagem de um perfil" data-testid="profile-top-btn" />
-      <h1 data-testid="page-title">{title}</h1>
-      {bool
-      && <img src={ searchIcon } alt="Imagem de uma lupa" data-testid="search-top-btn" />}
-    </header>
+    <>
+      <header>
+        <Link to="/profile">
+          <img
+            src={ profileIcon }
+            alt="Imagem de um perfil"
+            data-testid="profile-top-btn"
+          />
+        </Link>
+        <h1 data-testid="page-title">{title}</h1>
+        {bool
+        && <img
+          id="foo"
+          src={ searchIcon }
+          alt="Imagem de uma lupa"
+          data-testid="search-top-btn"
+        />}
+      </header>
+      {showInput
+      && <input
+        type="text"
+        data-testid="search-input"
+        placeholder="Pesquisar"
+      />}
+    </>
   );
 }
 
