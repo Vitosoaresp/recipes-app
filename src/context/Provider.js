c3ff4402b313f55be1cd1abd0cc73110d9dc4fde
Vitor Soares <pereiravitor1218@gmail.com>
2022-06-21 14:47:50 -0300

    faz requisição da api e salva no Provider

--- src/context/Provider.js
+++ src/context/Provider.js
@@ -1,16 +1,31 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import MyContext from './Context';
+import { fetchDrinks, fetchFoods } from '../services/fetchFoodsAndDrinks';
 
 function Provider({ children }) {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [drinksAPI, setDrinksAPI] = useState([]);
+  const [foodsAPI, setFoodsAPI] = useState([]);
+
+  useEffect(() => {
+    const fetchAPI = async () => {
+      const foods = await fetchFoods();
+      const drinks = await fetchDrinks();
+      setDrinksAPI(drinks);
+      setFoodsAPI(foods);
+    };
+    fetchAPI();
+  }, []);
 
   const context = {
     email,
     setEmail,
     password,
     setPassword,
+    drinksAPI,
+    foodsAPI,
   };
 
   return (
