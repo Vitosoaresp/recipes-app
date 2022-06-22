350642a3c5a5042fdbf462fcc9afe2dc0f983213
BiaKast <biahdrew94@gmail.com>
2022-06-22 14:41:54 -0300

    resolve conflitos 2

--- src/pages/Foods.jsx
+++ src/pages/Foods.jsx
@@ -1,5 +1,5 @@
-import Footer from '../components/Footer';
 import React, { useContext } from 'react';
+import Footer from '../components/Footer';
 import Header from '../components/Header';
 import MyContext from '../context/Context';
 
