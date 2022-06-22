8409a1c2a94a5b298efd2a897389eaa578ad503f
Breno Godoy <godoyy25z@gmail.com>
2022-06-22 17:53:32 -0300

    Requisito 47

--- src/pages/InProgressDrink.jsx
+++ src/pages/InProgressDrink.jsx
@@ -1,9 +1,69 @@
 import React from 'react';
+import { drink } from '../services/mockReturnApi';
 
 function InProgressDrink() {
+  const {
+    strDrinkThumb,
+    strDrink,
+    strCategory,
+    strInstructions,
+    strIngredient1,
+    strIngredient2,
+    strIngredient3,
+    strIngredient4,
+    strIngredient5,
+    strIngredient6,
+    strIngredient7,
+    strIngredient8,
+    strIngredient9,
+    strIngredient10,
+  } = drink;
+  const ingredientsArray = [
+    strIngredient1,
+    strIngredient2,
+    strIngredient3,
+    strIngredient4,
+    strIngredient5,
+    strIngredient6,
+    strIngredient7,
+    strIngredient8,
+    strIngredient9,
+    strIngredient10];
   return (
     <div>
-      <h1>InProgressDrink</h1>
+      <section>
+        <img src={ strDrinkThumb } alt="Imagem da receita" data-testid="recipe-photo" />
+        <h2 data-testid="recipe-title">{strDrink}</h2>
+        <p data-testid="recipe-category">{strCategory}</p>
+      </section>
+      <section>
+        <button type="button" data-testid="share-btn">Share</button>
+        <button type="button" data-testid="favorite-btn">Favorite</button>
+      </section>
+      <section>
+        <h2>Ingredients</h2>
+        <ul>
+          {ingredientsArray.map((element, i) => {
+            if (element === null || element === '') {
+              return null;
+            }
+            return (
+              <li
+                data-testid={ `${i}-ingredient-step` }
+                key={ i }
+              >
+                {element}
+              </li>);
+          })}
+        </ul>
+      </section>
+      <section>
+        <h2>Instructions</h2>
+        <p data-testid="instructions">{strInstructions}</p>
+      </section>
+      <section>
+        <button data-testid="finish-recipe-btn" type="button">Finish Recipe</button>
+      </section>
     </div>
   );
 }
--- src/pages/InProgressFood.jsx
+++ src/pages/InProgressFood.jsx
@@ -1,9 +1,69 @@
 import React from 'react';
+import { meal } from '../services/mockReturnApi';
 
 function InProgressFood() {
+  const {
+    strMealThumb,
+    strMeal,
+    strCategory,
+    strInstructions,
+    strIngredient1,
+    strIngredient2,
+    strIngredient3,
+    strIngredient4,
+    strIngredient5,
+    strIngredient6,
+    strIngredient7,
+    strIngredient8,
+    strIngredient9,
+    strIngredient10,
+  } = meal;
+  const ingredientsArray = [
+    strIngredient1,
+    strIngredient2,
+    strIngredient3,
+    strIngredient4,
+    strIngredient5,
+    strIngredient6,
+    strIngredient7,
+    strIngredient8,
+    strIngredient9,
+    strIngredient10];
   return (
     <div>
-      <h1>InProgressFood</h1>
+      <section>
+        <img src={ strMealThumb } alt="Imagem da receita" data-testid="recipe-photo" />
+        <h2 data-testid="recipe-title">{strMeal}</h2>
+        <p data-testid="recipe-category">{strCategory}</p>
+      </section>
+      <section>
+        <button type="button" data-testid="share-btn">Share</button>
+        <button type="button" data-testid="favorite-btn">Favorite</button>
+      </section>
+      <section>
+        <h2>Ingredients</h2>
+        <ul>
+          {ingredientsArray.map((element, i) => {
+            if (element === null || element === '') {
+              return null;
+            }
+            return (
+              <li
+                data-testid={ `${i}-ingredient-step` }
+                key={ i }
+              >
+                {element}
+              </li>);
+          })}
+        </ul>
+      </section>
+      <section>
+        <h2>Instructions</h2>
+        <p data-testid="instructions">{strInstructions}</p>
+      </section>
+      <section>
+        <button data-testid="finish-recipe-btn" type="button">Finish Recipe</button>
+      </section>
     </div>
   );
 }
