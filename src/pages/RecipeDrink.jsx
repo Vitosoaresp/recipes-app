a48a69769e5e266e13354e3b565ce81e0debe1fd
Vitor Soares <pereiravitor1218@gmail.com>
2022-06-22 17:30:59 -0300

    implementa os elementos nas telas de Recipe: Foods e Drink

--- src/pages/RecipeDrink.jsx
+++ src/pages/RecipeDrink.jsx
@@ -2,9 +2,27 @@ import React from 'react';
 
 function RecipeDrink() {
   return (
-    <div>
-      <h1>RecipeDrink</h1>
-    </div>
+    <main>
+      <div>
+        <div>
+          <img data-testid="recipe-photo" src="" alt="" />
+        </div>
+        <p data-testid="recipe-title">Title</p>
+        <button type="button" data-testid="share-btn">Compartilhar</button>
+        <button type="button" data-testid="favorite-btn">Favoritar</button>
+        <span data-testid="recipe-category">Categoria</span>
+        <ul>
+          Ingredientes
+          <li data-testid="{index}-ingredient-name-and-measure">Ingrediente 1</li>
+        </ul>
+        <p data-testid="instructions">Instrução</p>
+        <div>
+          receitas recomendadas
+          {/* data-testid="${index}-recomendation-card" */}
+        </div>
+        <button type="button" data-testid="start-recipe-btn">Iniciar receita</button>
+      </div>
+    </main>
   );
 }
 
