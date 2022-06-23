295aa031d4c1d64a525212e1a3dc1d7f1e2d9903
jeffersonalmeida <jeffim.git@gmail.com>
2022-06-23 02:30:29 -0400

    Requisito 64 ao 72

--- src/pages/ExploreDrink.jsx
+++ src/pages/ExploreDrink.jsx
@@ -1,11 +1,35 @@
 import React from 'react';
+import { Link, useHistory } from 'react-router-dom';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
 
+const RANDOM_DRINK_URL = 'https://www.thecocktaildb.com/api/json/v1/1/random.php';
+
 function ExploreDrink() {
+  const history = useHistory();
+
+  const goToRandomDrink = async () => {
+    const response = await fetch(RANDOM_DRINK_URL);
+    const { drinks } = await response.json();
+    if (drinks[0].idDrink) history.push(`/drinks/${drinks[0].idDrink}`);
+  };
+
   return (
     <div>
-      <Header title="Explore Drinks" />
+      <Header pageTitle="Explore Drinks" />
+      <Link to="/explore/drinks/ingredients">
+        <button type="button" data-testid="explore-by-ingredient">
+          By Ingredient
+        </button>
+      </Link>
+
+      <button
+        type="button"
+        data-testid="explore-surprise"
+        onClick={ goToRandomDrink }
+      >
+        Surprise me!
+      </button>
       <Footer />
     </div>
   );
--- src/pages/ExploreFood.jsx
+++ src/pages/ExploreFood.jsx
@@ -1,11 +1,49 @@
 import React from 'react';
+import { Link, useHistory } from 'react-router-dom';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
 
+const RANDOM_FOOD_URL = 'https://www.themealdb.com/api/json/v1/1/random.php';
+
 function ExploreFood() {
+  const history = useHistory();
+
+  const goToRandomMeal = async () => {
+    const response = await fetch(RANDOM_FOOD_URL);
+    const { meals } = await response.json();
+    // setRandomMealId(meals[0].idMeal);
+    if (meals[0].idMeal) history.push(`/foods/${meals[0].idMeal}`);
+  };
+
   return (
     <div>
       <Header title="Explore Foods" />
+      <Link to="/explore/foods/ingredients">
+        <button
+          type="button"
+          data-testid="explore-by-ingredient"
+        >
+          By Ingredient
+        </button>
+      </Link>
+
+      <Link to="/explore/foods/nationalities">
+        <button
+          type="button"
+          data-testid="explore-by-nationality"
+        >
+          By Nationality
+        </button>
+      </Link>
+
+      <button
+        type="button"
+        data-testid="explore-surprise"
+        onClick={ goToRandomMeal }
+      >
+        Surprise me!
+      </button>
+
       <Footer />
     </div>
   );
