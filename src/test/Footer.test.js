feccdb774c59299a4142b20c119d01935665784e
BiaKast <biahdrew94@gmail.com>
2022-06-23 19:13:40 -0300

    corrigndo erro eslint

--- src/test/Footer.test.js
+++ src/test/Footer.test.js
@@ -1,9 +1,14 @@
-// import React from 'react';
-// import { screen } from '@testing-library/react';
+import React from 'react';
+import { screen } from '@testing-library/react';
 // import userEvent from '@testing-library/user-event';
-// import renderWithRouter from './renderWithRouter';
-// import App from '../App';
+import renderWithRouter from './renderWithRouter';
+import App from '../App';
 
-// describe('Testa tela de Login', () => {
-
-// });
+describe('Testa tela de receitas feitas', () => {
+  const DRINK_TEST = 'drinks-bottom-btn';
+  test('Testa se os icones estão corretos', () => {
+    renderWithRouter(<App />);
+    const imgDrinkIcon = screen.getByTestId(DRINK_TEST);
+    expect(imgDrinkIcon).toBeDefined();
+  });
+});
