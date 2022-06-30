fa4cdba394e49bffb52244dfd57737c28989f0b3
BiaKast <biahdrew94@gmail.com>
2022-06-30 18:07:56 -0300

    css profile

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -3,40 +3,46 @@ import { Link, useHistory } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
+import styles from '../modules/Profile.module.css';
 
 function Profile() {
   const history = useHistory();
-  const email = localStorage.getItem('user');
+  const { email } = JSON.parse(localStorage.getItem('user'));
   return (
     <div>
       <Header title="Profile" />
-      <h3 data-testid="profile-email">{email}</h3>
-      <Link to="/done-recipes">
+      <div className={ styles.profile }>
+        <h3 data-testid="profile-email">{email}</h3>
+        <Link className={ styles.category } to="/done-recipes">
+          <button
+            className={ styles.buttonLink }
+            type="button"
+            data-testid="profile-done-btn"
+          >
+            Done Recipes
+          </button>
+        </Link>
+        <Link className={ styles.category } to="/favorite-recipes">
+          <button
+            className={ styles.buttonLink }
+            type="button"
+            data-testid="profile-favorite-btn"
+          >
+            Favorite Recipes
+          </button>
+        </Link>
         <button
+          className={ styles.category }
           type="button"
-          data-testid="profile-done-btn"
+          data-testid="profile-logout-btn"
+          onClick={ () => {
+            localStorage.clear();
+            history.push('/');
+          } }
         >
-          Done Recipes
+          Logout
         </button>
-      </Link>
-      <Link to="/favorite-recipes">
-        <button
-          type="button"
-          data-testid="profile-favorite-btn"
-        >
-          Favorite Recipes
-        </button>
-      </Link>
-      <button
-        type="button"
-        data-testid="profile-logout-btn"
-        onClick={ () => {
-          localStorage.clear();
-          history.push('/');
-        } }
-      >
-        Logout
-      </button>
+      </div>
       <Footer />
     </div>
   );
